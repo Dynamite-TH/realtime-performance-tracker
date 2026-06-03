d20271be6ffc453c42fb607873d522ffedd1484e
Dynamite-TH <tedhayward@icloud.com>
2026-06-03 01:48:48 +0100

    Basic memory tracker implemented - console.log to check functionality with 30  min delay

--- agent.js
+++ agent.js
@@ -1,1 +1,11 @@
+const os = require('os');
 
+const interval = 1800000 // 18000 seconds / 30 mins
+
+setInterval(() => {
+    let totalMemory = os.totalmem()
+    let freeMemory = os.freemem()
+    const memoryUsage = 100 - ((freeMemory / totalMemory) * 100)
+    console.log(`Total Number of memory used: ${memoryUsage.toFixed(2)}% taken every ${60000 / 1000} seconds`)
+    
+}, 60000);
