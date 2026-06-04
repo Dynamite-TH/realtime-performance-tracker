dacd5de02fa1e08d773ce8bc984cad48fdd27716
Dynamite-TH <tedhayward@icloud.com>
2026-06-04 17:12:39 +0100

    Add cpu idle function to calculate the idle and working time of a cpu to work out the cpu uage

--- agent/agent.js
+++ agent/agent.js
@@ -1,25 +1,51 @@
 const os = require('os');
+const { cpuUsage } = require('process');
 
 let URL = 'http://localhost:3000/api/metrics'
 
 const interval = 2000 // 18000 seconds / 30 mins
 
-function time() {
-    const date = new Date()
-    const seconds = date.getSeconds()
-    const minutes = date.getMinutes()
-    const hour = date.getHours()
-    const day = date.getDay()
+function cpuIdle() {
+    const cpus = os.cpus();
+    let totalMs = 0;
+    let idleMs = 0;
 
-    return `${day}:${hour}:${minutes}:${seconds}`
+    cpus.forEach((core) => {
+        for (type in core.times) {
+            totalMs += core.times[type];
+        }
+        idleMs += core.times.idle;
+    });
 
+    return { totalMs, idleMs };
 }
 
-const intervalId = setInterval(() => {
+function getCpuUsage() {
+    return new Promise((resolve) => {
+        const start = cpuIdle();
+
+        setTimeout(() => {
+            const end = cpuIdle();
+
+            const idle = end.idleMs - start.idleMs;
+            const total = end.totalMs - start.totalMs;
+
+            // Calculate the percentage of time the CPU was NOT idle
+            const cpuPercentage = (1 - idle / total) * 100;
+
+            resolve(Number(cpuPercentage)); // Format to 2 decimal places
+        }, 100); // 100ms sample window
+    });
+}
+
+const intervalId = setInterval(async () => {
+    const cpuUsage = await getCpuUsage()
     let totalMemory = os.totalmem()
     let freeMemory = os.freemem()
-    const memoryUsage = 100 - ((freeMemory / totalMemory) * 100).toFixed(2)
-    console.log(`Total Number of memory used: ${memoryUsage.toFixed(2)}% taken every ${2000 / 1000} seconds, Taken at ${time()}`);
+    const memoryUsage = (100 - ((freeMemory / totalMemory) * 100))
+    const uptime = os.uptime()
+    console.log(`Total Number of memory used: ${memoryUsage}%, total cpu usage ${cpuUsage}%, Taken at ${new Date()}`);
+
 
     (async () => {
         try {
@@ -30,8 +56,9 @@ const intervalId = setInterval(() => {
                 },
                 body: JSON.stringify({
                     timestamp: new Date(),
-                    cpuUsage: 0, // set to 0 for now while implementation is being done
-                    memoryUsagePercentage: memoryUsage
+                    cpuUsage: cpuUsage, // set to 0 for now while implementation is being done
+                    memoryUsagePercentage: memoryUsage,
+                    system_uptime: uptime
                 })
             })
 
