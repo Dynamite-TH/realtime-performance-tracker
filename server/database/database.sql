2dfa22c17efb52d0a3a97bc00724065efaf549ec
Dynamite-TH <tedhayward@icloud.com>
2026-06-17 19:42:56 +0100

    Enhance server_health table structure and materialized view for improved data aggregation

--- server/database/database.sql
+++ server/database/database.sql
@@ -1,28 +1,34 @@
-CREATE TABLE server_health (
+CREATE EXTENSION IF NOT EXISTS timescaledb;
+
+CREATE TABLE IF NOT EXISTS server_health (
     time TIMESTAMPTZ NOT NULL,
-    server_id VARCHAR(50) NOT NULL, 
     cpu_usage DOUBLE PRECISION,
-    memory_percentage DOUBLE PRECISION
+    memory_total_bytes BIGINT,
+    memory_free_bytes BIGINT,
+    memory_usage_percent DOUBLE PRECISION,
+    disk_size_bytes BIGINT,
+    disk_usage_percent DOUBLE PRECISION,
+    system_uptime_seconds DOUBLE PRECISION
 );
 
+SELECT create_hypertable('server_health', 'time', if_not_exists => TRUE);
 
-SELECT create_hypertable('server_health', 'time');
-
-CREATE MATERIALIZED VIEW server_health_hourly_avg
+CREATE MATERIALIZED VIEW IF NOT EXISTS server_health_hourly_avg
 WITH (timescaledb.continuous) AS
 SELECT
     time_bucket('1 hour', time) AS hourly_bucket,
-    server_id,
     AVG(cpu_usage) AS avg_cpu,
     MAX(cpu_usage) AS max_cpu,
-    AVG(memory_percentage) AS avg_memory
+    AVG(memory_usage_percent) AS avg_memory,
+    AVG(disk_usage_percent) AS avg_disk,
+    AVG(system_uptime_seconds) AS avg_uptime
 FROM server_health
-GROUP BY hourly_bucket, server_id;
+GROUP BY hourly_bucket;
 
 SELECT add_continuous_aggregate_policy('server_health_hourly_avg',
     start_offset => INTERVAL '8 days',
-    end_offset => INTERVAL '1 hour',      
-    schedule_interval => INTERVAL '1 hour' 
+    end_offset => INTERVAL '1 hour',
+    schedule_interval => INTERVAL '1 hour'
 );
 
 SELECT add_retention_policy('server_health', INTERVAL '7 days');
