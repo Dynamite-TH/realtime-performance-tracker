98d9bc7f3b97c0e38e6d23c05ac107fda1d89dba
Dynamite-TH <tedhayward@icloud.com>
2026-06-17 18:21:13 +0100

    Update requirements and enhance server_health table structure for improved data aggregation

--- server/database/database.sql
+++ server/database/database.sql
@@ -1,8 +1,10 @@
 CREATE TABLE server_health (
     time TIMESTAMPTZ NOT NULL,
+    server_id VARCHAR(50) NOT NULL, 
     cpu_usage DOUBLE PRECISION,
     memory_percentage DOUBLE PRECISION
 );
+
 
 SELECT create_hypertable('server_health', 'time');
 
@@ -10,15 +12,17 @@ CREATE MATERIALIZED VIEW server_health_hourly_avg
 WITH (timescaledb.continuous) AS
 SELECT
     time_bucket('1 hour', time) AS hourly_bucket,
+    server_id,
     AVG(cpu_usage) AS avg_cpu,
+    MAX(cpu_usage) AS max_cpu,
     AVG(memory_percentage) AS avg_memory
 FROM server_health
-GROUP BY hourly_bucket;
+GROUP BY hourly_bucket, server_id;
 
 SELECT add_continuous_aggregate_policy('server_health_hourly_avg',
-    start_offset => INTERVAL '3 hours',  -- How far back to check for new/delayed raw data
-    end_offset => INTERVAL '1 hour',     -- Don't aggregate the current, active hour yet
-    schedule_interval => INTERVAL '1 hour' -- Run this maintenance window every hour
+    start_offset => INTERVAL '8 days',
+    end_offset => INTERVAL '1 hour',      
+    schedule_interval => INTERVAL '1 hour' 
 );
 
 SELECT add_retention_policy('server_health', INTERVAL '7 days');
