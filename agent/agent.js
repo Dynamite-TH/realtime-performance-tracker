19f38be14a51943c4a7ebf2a513e0437e3a0ea3f
Dynamite-TH <tedhayward@icloud.com>
2026-06-17 13:16:09 +0100

    Setting up agent to save a backlog when running so in the case of a server outage, it will save that data and push when back online

--- agent/agent.js
+++ agent/agent.js
@@ -51,6 +51,7 @@ async function getDiskSize() {
 }
 const diskSize = await getDiskSize()
 const totalMemory = totalmem()
+let localQueue = [];
 
 const intervalId = setInterval(async () => {
     const cpuUsage = await getCpuUsage()
@@ -64,35 +65,53 @@ const intervalId = setInterval(async () => {
     const uptime = _uptime()
     console.log(`Total Number of memory used: ${memoryUsage}%, total cpu usage ${cpuUsage}%, avaialble bytes ${diskUsage}, total bytes ${diskSize}, storage used ${diskUsage}%, Taken at ${new Date()}, `);
 
+    let payload = {
+        timestamp: new Date(),
+        cpuUsage: cpuUsage,
+        memory: { totalMemory: totalMemory, freeMemory: freeMemory, memoryUsage: memoryUsage },
+        disk: { diskUsage: diskUsage, diskSize: diskSize, diskUsage: diskUsage },
+        system_uptime: uptime
+    }
 
-    (async () => {
-        try {
-            const response = await fetch(URL, {
-                method: "POST",
-                headers: {
-                    "Content-Type": "application/json"
-                },
-                body: JSON.stringify({
-                    timestamp: new Date(),
-                    cpuUsage: cpuUsage,
-                    memory: { totalMemory: totalMemory, freeMemory: freeMemory, memoryUsage: memoryUsage },
-                    disk: { diskUsage: diskUsage, diskSize: diskSize, diskUsage: diskUsage },
-                    system_uptime: uptime
-
+        (async () => {
+            try {
+                if (localQueue.length > 0) {
+                    console.log("sending existing backlog payload")
+                    const response = await fetch(URL, {
+                        method: "POST",
+                        headers: {
+                            "Content-Type": "application/json"
+                        },
+                        body: JSON.stringify(localQueue)
+                    })
+                    if (response.ok) {
+                        localQueue = [] //local queue needs to be flushed after pushed
+                    }
+                }
+                const response = await fetch(URL, {
+                    method: "POST",
+                    headers: {
+                        "Content-Type": "application/json"
+                    },
+                    body: JSON.stringify(payload)
                 })
-            })
 
-            if (!response.ok) {
-                console.error(`Server responded with status ${response.status}; stopping metrics interval.`)
-                clearInterval(intervalId)
-                return
+                if (!response.ok) {
+                    console.error(`Server responded with status ${response.status}; stopping metrics interval.`)
+                }
+
+                console.log(`Metrics sent with status ${response.status}`)
+            } catch (e) {
+                const MAX_QUEUE_SIZE = 10000 //average around 5.5 hours of data that can be saved
+                console.error('Error sending metrics;', (e.message))
+                if (localQueue.length >= MAX_QUEUE_SIZE) {
+                    // Drop the OLDEST data point (index 0) to free up space
+                    localQueue.shift();
+                    console.warn('Queue limit reached! Dropped oldest metric to save RAM.');
+                }
+                localQueue.push()
+
             }
-
-            console.log(`Metrics sent with status ${response.status}`)
-        } catch (e) {
-            console.error('Error sending metrics; stopping interval.', e)
-            clearInterval(intervalId)
-        }
-    })()
+        })()
 
 }, interval);
