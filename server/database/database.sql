02e1f468d77b2f736bead126f3f42fac9b6adae4
Dynamite-TH <tedhayward@icloud.com>
2026-08-02 22:59:21 +0100

    Remove TimescaleDB extension and related continuous aggregate policies from database schema as data insertion has changed and been more streamlined

--- server/database/database.sql
+++ server/database/database.sql
@@ -1,35 +1,7 @@
-CREATE EXTENSION IF NOT EXISTS timescaledb;
-
 CREATE TABLE IF NOT EXISTS server_health (
     time TIMESTAMPTZ NOT NULL,
     cpu_usage DOUBLE PRECISION,
-    memory_total_bytes BIGINT,
-    memory_free_bytes BIGINT,
     memory_usage_percent DOUBLE PRECISION,
-    disk_size_bytes BIGINT,
     disk_usage_percent DOUBLE PRECISION,
     system_uptime_seconds DOUBLE PRECISION
 );
-
-SELECT create_hypertable('server_health', 'time', if_not_exists => TRUE);
-
-CREATE MATERIALIZED VIEW IF NOT EXISTS server_health_hourly_avg
-WITH (timescaledb.continuous) AS
-SELECT
-    time_bucket('1 hour', time) AS hourly_bucket,
-    COUNT(*) AS record_count,
-    AVG(cpu_usage) AS avg_cpu,
-    MAX(cpu_usage) AS max_cpu,
-    AVG(memory_usage_percent) AS avg_memory,
-    AVG(disk_usage_percent) AS avg_disk,
-    AVG(system_uptime_seconds) AS avg_uptime
-FROM server_health
-GROUP BY hourly_bucket;
-
-SELECT add_continuous_aggregate_policy('server_health_hourly_avg',
-    start_offset => INTERVAL '5 days',
-    end_offset => INTERVAL '1 hour',
-    schedule_interval => INTERVAL '1 hour'
-);
-
-SELECT add_retention_policy('server_health', INTERVAL '7 days');
